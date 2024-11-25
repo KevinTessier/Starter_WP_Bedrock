db410da0499693cb93a8dd7a1043fb9f9a010714
Kévin Tessier <kevin-tessier@protonmail.com>
2024-11-25 15:14:32 +0100

    add : config starter theme

---
+++ web/app/themes/Starter_ThemeWP_Sage/resources/views/components/navquickly.blade.php
@@ -0,0 +1,6 @@
+<nav id="access-fast" role="navigation" aria-label="Menu d'accès rapide">
+    <ul class="list-none">
+        <li class="list-none"><a href="#main-menu" title="Accèder au menu principal">Menu principal</a></li>
+        <li class="list-none"><a href="#content-primary" title="Accèder au contenu">Aller directement au contenu principal</a></li>
+    </ul>
+</nav>
